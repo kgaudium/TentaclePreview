5f380d684640b962a5b61043732724948e378f5f
kgaudium <k.gaudium@gmail.com>
2025-07-27 19:31:54 +0300

    ref(output): remove old `progress` function implementation

--- TentaclePreview/output.py
+++ TentaclePreview/output.py
@@ -17,7 +17,7 @@ COLORS = {
 PREFIXES = {
     "info": "ℹ️  [INFO]\t",  # Синий информационный
     "success": "✅ [SUCCESS]\t",  # Зелёный успех
-    "warning": "⚠️ [WARNING]\t",  # Жёлтое предупреждение
+    "warning": "⚠️  [WARNING]\t",  # Жёлтое предупреждение
     "error": "❌ [ERROR]\t",  # Красная ошибка
     "header": "📌 [SECTION]\t"
 }
@@ -36,62 +36,6 @@ def log(message, status: Literal["info", "success", "warning", "error", "header"
 
     print(f"{color}{prefix} [{time_now}] {message}{COLORS['reset']}", **kwargs)
 
-
-# progress_globals = {
-#     "last_current": 0,
-#     "is_new_progress": True,
-#     "anim": ['\\', '|', '/', '―'],
-#     "is_done": False,
-# }
-# def progress(op_code, cur_count, max_count=None, message=''):
-#     global progress_globals, ENABLED_LOG_LEVELS
-#
-#     if (ENABLED_LOG_LEVELS != "all") and ("all" not in ENABLED_LOG_LEVELS):
-#         if "progressbar" not in ENABLED_LOG_LEVELS:
-#             return
-#
-#     if cur_count < progress_globals["last_current"]:
-#         progress_globals["is_new_progress"] = True
-#         progress_globals["is_done"] = False
-#
-#     clear_line_code = "\033[A"
-#     done_color = "\033[36m"
-#     pending_color = "\033[90m"
-#     reset_color = "\033[0m"
-#     progress_char = '■'
-#     done_symbol = '✓'
-#     max_length = 30
-#
-#     if progress_globals["is_new_progress"]:
-#         print()
-#
-#     print(f"{clear_line_code}", end="")
-#     result = ""
-#
-#     if progress_globals["is_done"]:
-#         result += f"{done_symbol} "
-#     else:
-#         result += f"{progress_globals['anim'][cur_count % len(progress_globals['anim'])]} "
-#
-#     if max_count is not None:
-#         done_count = int((cur_count / max_count) * max_length)
-#         pending_count = max_length - done_count
-#
-#         result += f"{done_color}{progress_char*done_count}"
-#         result += f"{pending_color}{progress_char*pending_count}"
-#         result += f"{reset_color} {message}"
-#
-#         if not progress_globals["is_done"]:
-#             print(result, end="")
-#
-#         if pending_count == 0 and not progress_globals["is_done"]:
-#             progress_globals["is_done"] = True
-#             print(f"{clear_line_code}")
-#             print(f"{done_symbol} {done_color}{progress_char*done_count}{reset_color} {message}")
-#     else:
-#         pass
-#
-#     progress_globals["last_current"] = cur_count
 
 anim = ['\\ ', '| ', '/ ', '- ']
 anim_index = 0
